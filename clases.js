4bd0f9ba5713868bf99d297a845acebf0756489a
Darcano <pinillapuerta111@hotmail.com>
2022-07-18 15:48:49 -0500

    add module

---
+++ clases.js
@@ -0,0 +1,6 @@
+export default class Proceso {
+    constructor(pid, nombre) {
+        this.pid = pid;
+        this.nombre = nombre;
+    }
+}
---
+++ script.js
@@ -0,0 +1,12 @@
+import Proceso from "./clases.js"
+
+window.prueba = function prueba() {
+    console.log("Si funciona");
+
+    const proNote = new Proceso(
+        "p1",
+        "Notepad"
+    );
+
+    console.log("Prueba:",Proceso)
+}
