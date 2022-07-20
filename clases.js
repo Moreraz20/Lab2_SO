43cb4e9cb8b3029761d4796ce0365ae6807c54d6
Darcano <pinillapuerta111@hotmail.com>
2022-07-20 13:39:53 -0500

    add tempo

--- clases.js
+++ clases.js
@@ -1,6 +1,128 @@
 export default class Proceso {
-    constructor(pid, nombre) {
-        this.pid = pid;
-        this.nombre = nombre;
+  constructor(nombre, llegada, ejecucion, bloqueo, estado) {
+    this.nombre = nombre;
+    this.llegada = llegada;
+    this.ejecucion = ejecucion;
+    this.bloqueo = bloqueo;
+    this.estado = estado;
+    this.aux = {llegada: llegada, ejecucion: ejecucion, bloqueo: bloqueo};
+  }
+
+  agregarTabla(tabla) {
+    const tr = document.createElement("tr");
+
+    const td2 = document.createElement("td");
+    td2.innerHTML = this.nombre; //Columna Nombre
+
+    const td3 = document.createElement("td");
+    td3.innerHTML = this.llegada; //Columna llegada
+
+    const td4 = document.createElement("td");
+    td4.innerHTML = this.ejecucion; //Columna ejecucion
+
+    const td5 = document.createElement("td");
+    td5.innerHTML = this.bloqueo.inicio; //Columna inicio bloqueo
+
+    const td6 = document.createElement("td");
+    td6.innerHTML = this.bloqueo.duracion; //Columna duracion bloqueo
+
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    tr.appendChild(td6);
+
+    tabla.appendChild(tr);
+  }
+
+  eliminarTabla(tablaAntigua) {
+    while (tablaAntigua.firstChild) {
+      tablaAntigua.removeChild(tablaAntigua.firstChild);
     }
+  }
+}
+
+export class CPU {
+  constructor(
+    encendido,
+    desocupada,
+    promRetorno,
+    promEjecucion,
+    promEspera,
+    promTiempoPerdido
+  ) {
+    this.encendido = encendido;
+    this.desocupada = desocupada;
+    this.tiempoTotal = encendido - desocupada;
+    this.promRetorno = promRetorno;
+    this.promEjecucion = promEjecucion;
+    this.promEspera = promEspera;
+    this.promTiempoPerdido = promTiempoPerdido;
+  }
+}
+
+export class Grafico {
+  constructor() {
+  }
+
+  crearProceso(listProcesos,grafico) {
+    listProcesos.forEach(element => {
+      const div = document.createElement("div");
+      div.id = "grafico_proceso_" + element.nombre;
+      div.setAttribute("class","proceso");
+
+      const divNombre = document.createElement("div");
+      divNombre.innerHTML = element.nombre;
+
+      div.appendChild(divNombre);
+
+      grafico.appendChild(div);
+    });
+  }
+
+  eliminarProceso(grafico) {
+    while (grafico.firstChild) {
+      grafico.removeChild(grafico.firstChild);
+    }
+  }
+
+  agregarEstado(proceso,tiempo,estado){
+    const divContenedor = document.getElementById("grafico_proceso_"+proceso.nombre);
+
+    const divEstado = document.createElement("div");
+    divEstado.id = "estado_"+proceso.nombre+"_"+(tiempo+2)
+
+    divEstado.setAttribute("class",estado);
+
+    divEstado.setAttribute("style","grid-column: "+(tiempo+2)+";")
+    divContenedor.appendChild(divEstado);
+    /**************************************************************************************************************************/
+  }
+
+  sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+     if ((new Date().getTime() - start) > milliseconds) {
+      console.log("Final:",new Date().getTime(),"inicial", start);
+      break;
+     }
+    }
+   }
+}
+
+export class FCFS {
+  constructor(listProcesos,grafico){
+    this.listProcesos = listProcesos;
+    this.grafico = grafico;
+  }
+
+  ejecutar(tiempo){
+    this.listProcesos.forEach(element => {
+      if(element.llegada == tiempo){
+        this.grafico.agregarEstado(element,tiempo,"ejecucion");
+      }else{
+        this.grafico.agregarEstado(element,tiempo,"vacio");
+      }
+    });
+  }
 }
--- script.js
+++ script.js
@@ -1,12 +1,53 @@
-import Proceso from "./clases.js"
+import Proceso, { CPU, FCFS, Grafico } from "./clases.js";
 
 window.prueba = function prueba() {
-    console.log("Si funciona");
+  console.log("Si funciona");
+};
 
-    const proNote = new Proceso(
-        "p1",
-        "Notepad"
-    );
+const procesoA = new Proceso("A", 0, 6, { inicio: 3, duracion: 2 }, "Existe");
+const procesoB = new Proceso("B", 1, 8, { inicio: 1, duracion: 3 }, "Existe");
+const procesoC = new Proceso("C", 2, 7, { inicio: 5, duracion: 1 }, "Existe");
+const procesoD = new Proceso("D", 4, 3, { inicio: 0, duracion: 0 }, "Existe");
+const procesoE = new Proceso("E", 6, 9, { inicio: 2, duracion: 4 }, "Existe");
+const procesoF = new Proceso("F", 6, 2, { inicio: 0, duracion: 0 }, "Existe");
 
-    console.log("Prueba:",Proceso)
+let listaProcesos = [
+  procesoA,
+  procesoB,
+  procesoC,
+  procesoD,
+  procesoE,
+  procesoF,
+];
+
+const tabla = document.getElementById("tablaProcesos");
+
+listaProcesos.forEach((element) => {
+  element.agregarTabla(tabla);
+});
+
+const grafico = new Grafico();
+
+const contenedorGrafico = document.getElementById("contenedorGrafico");
+
+grafico.crearProceso(listaProcesos, contenedorGrafico);
+
+const fcfs = new FCFS(listaProcesos, grafico);
+for (let index = 0; index < 10; index++) {
+  //fcfs.ejecutar(index);
+  
+  //grafico.sleep(1000);
+}
+let identificardor = setInterval(tempo,1000);
+
+let cont = 0;
+
+
+function tempo(){
+  fcfs.ejecutar(cont)
+  cont++;
+  console.log("Contado",cont);
+  if(cont==10){
+    clearInterval(identificardor);
+  }
 }
